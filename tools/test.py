b5ed59b33a57a73d3b692d482bc2c0e7bc653685
Tianning Yuan <47728709+yuantn@users.noreply.github.com>
2021-04-20 15:42:28 +0800

    Fix test bugs

--- tools/test.py
+++ tools/test.py
@@ -127,8 +127,9 @@ def main():
 
     if not distributed:
         model = MMDataParallel(model, device_ids=[0])
-        outputs = single_gpu_test(model, data_loader, args.show, args.show_dir,
-                                  args.show_score_thr)
+        outputs = single_gpu_test(model, data_loader
+#                                   , args.show, args.show_dir, args.show_score_thr
+                                 )
     else:
         model = MMDistributedDataParallel(
             model.cuda(),
