a38d9bc767acdd21588c38bbf216ddc2b798e282
Tianning Yuan <47728709+yuantn@users.noreply.github.com>
2021-08-03 11:36:51 +0800

    Add ssd_head registry

--- mmdet/models/dense_heads/__init__.py
+++ mmdet/models/dense_heads/__init__.py
@@ -1,6 +1,7 @@
 from .MIAOD_head import MIAODHead
 from .MIAOD_retina_head import MIAODRetinaHead
+from .ssd_head import SSDHead
 
 __all__ = [
-    'MIAODHead', 'MIAODRetinaHead'
+    'MIAODHead', 'MIAODRetinaHead', 'SSDHead'
 ]
