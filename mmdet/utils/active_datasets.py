0a0b257fe3d20b33ea6ace447d4c7480270581e0
Tianning Yuan <47728709+yuantn@users.noreply.github.com>
2021-10-28 10:32:23 +0800

    Fix bugs when X_L_next > X_U_next

--- mmdet/utils/active_datasets.py
+++ mmdet/utils/active_datasets.py
@@ -67,6 +67,9 @@ def update_X_L(uncertainty, X_all, X_L, X_S_size):
     all_X_U_next = np.array(list(set(X_all) - set(X_L_next)))
     np.random.shuffle(all_X_U_next)
     X_U_next = all_X_U_next[:X_L_next.shape[0]]
+    if X_L_next.shape[0] > X_U_next.shape[0]:
+        np.random.shuffle(X_L_next)
+        X_U_next = np.concatenate((X_U_next, X_L_next[:X_L_next.shape[0] - X_U_next.shape[0]]))
     X_L_next.sort()
     X_U_next.sort()
     return X_L_next, X_U_next
